7d97d9b410ce8997d2a9b121c6f692a94e43fdc9
Peabrain <peabrainc64@hotmail.com>
2015-11-25 00:02:13 +0100

    a

--- Semester1/Aufgaben/BetriebsKommSysteme/Uebung5/processAdminFrame.c
+++ Semester1/Aufgaben/BetriebsKommSysteme/Uebung5/processAdminFrame.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /*Implementierung einer Prozessverwaltung:
 
 Implementieren Sie die durch das Framework vorgegebene Prozessverwaltung,
 die eine Menge von Prozessen aus der Datei prcs.dat auslesen,und diese
 in eine doppelt verkettete Liste einfuegen soll.
 
-Nun soll Prozess um Prozess der Reihenfolge ihrer ids entsprechend 
+Nun soll Prozess um Prozess der Reihenfolge ihrer ids entsprechend
 aus der Liste entfernt werden,wobei die uebrig gebliebenen Prozesse in jeder
 Iteration ausgegeben werden.
 
 Die Prozesse sind in der Datei prcs.dat Zeile fuer Zeile in der Form:
     ProzessId,Ankunftszeit,Ausfuehrungszeit
-gespeichert.    
+gespeichert.
 
 */
 
@@ -24,6 +23,8 @@ struct pData //Daten der doppelt verketteten Liste
  int aTime;//Ankunftszeit
  int sTime;//Ausfuehrungszeit
 
+ struct pData *next;
+ struct pData *prev;
   /*Struktur vervollstaendigen */
 };
 
@@ -44,17 +45,22 @@ void readProcesses(LINK head);//Einlesen der Prozesse
 
 int main(void)
 {
- LINK next;
- LINK head;
+ LINK next = 0;
+ LINK head = 0;
 
 /*TODO:head initialisieren*/
- 
+	head = (LINK)malloc(sizeof(PROCESS));
+	head->pId = 0;
+	head->next = head;
+	head->prev = head;
+
  readProcesses(head);
- while(head->next!=head)
+
+  while(head->next!=head)
  {
-   printList(head);                     
+   printList(head);
    next=findNextProcess(head);
-   deleteProcess(next);                   
+   deleteProcess(next);
  }
 
 return 0;
@@ -68,6 +74,26 @@ Implementierung der Methoden*/
 void readProcesses(LINK head)
 {
  /*TODO:implementieren*/
+ char linebuffer[256];
+ int i = 0;
+ FILE *f = fopen("prcs.dat","r");
+ if(f != 0)
+ {
+ 	while(fgets(linebuffer,256,f))
+	{
+		int a,b,c;
+		if(sscanf(linebuffer,"%i,%i,%i",&a,&b,&c) == 3)
+		{
+			LINK h = (LINK)malloc(sizeof(PROCESS));
+			h->pId = a;
+			h->aTime = b;
+			h->sTime = c;
+			addProcess(head,h);
+		}
+	}
+ 	fclose(f);
+ }
+
 }
 
 
@@ -75,12 +101,24 @@ void readProcesses(LINK head)
 void addProcess(LINK head,LINK neu)
 {
  /*TODO:implementieren*/
+	LINK prev = head->prev;
+	neu->next = head;
+	prev->next = neu;
+	neu->prev = prev;
+	head->prev = neu;
+	printf("add process: %i,%i,%i\n",neu->pId,neu->aTime,neu->sTime);
 }
 
 //Loeschen des angegebenen Knotens
 void deleteProcess(LINK current)
 {
  /*TODO:implementieren*/
+	LINK prev = current->prev;
+	LINK next = current->next;
+
+	prev->next = next;
+	next->prev = prev;
+	free(current);
 }
 
 
@@ -89,6 +127,7 @@ void deleteProcess(LINK current)
 LINK findNextProcess(LINK head)
 {
  /*TODO:implementieren*/
+ return head->next;
 }
 
 
@@ -103,10 +142,10 @@ void printList(LINK head)
      printf("\n\n Current QUEUE of PROCESSES ---------------------------------------\n\n");
      LINK curr=head->next;
      while(curr!=head)
-     {           
-      printPr(curr);  
-      curr=curr->next;                     
-     }     
+     {
+      printPr(curr);
+      curr=curr->next;
+     }
 }
 
 

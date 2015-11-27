2e801a56d2e9654e781e3941e8fad66331ef9d0c
Peabrain <peabrainc64@hotmail.com>
2015-11-27 21:59:20 +0100

    l

---
+++ Semester1/Aufgaben/Rechnerarchitektur/Uebung6/stack_.asm
@@ -0,0 +1,31 @@
+; Rechnerarchitektur
+; Übung 6
+;
+; Andreas Timmermann, Shima Shahadifar
+; MtrNr: 4994606, 5018598
+; Gruppe 2
+
+;====================================
+section .text			; Codesegment
+global _init			; die Funktion 'init' global setzen
+global _push			; die Funktion 'push' global setzen
+global _pop				; die Funktion 'pop' global setzen
+global _top				; die Funktion 'top' global setzen
+global _isEmpty			; die Funktion 'isEmpty' global setzen
+;====================================
+_init:
+	ret
+;====================================
+_push:
+	ret
+;====================================
+_pop:
+	ret
+;====================================
+_top:
+	ret
+;====================================
+_isEmpty:
+	ret
+;====================================
+

75445fef694733571d3b080e2ca0414239822ed4
Peabrain <peabrainc64@hotmail.com>
2015-12-12 23:57:54 +0100

    j

---
+++ Semester1/Aufgaben/Rechnerarchitektur/Uebung8/bubblesort.c
@@ -0,0 +1,13 @@
+//Fabien Geeraert und Ailis Oßwald, Tutorium 02 bei Christoph Van Heteren-Frese
+#include <stdio.h>
+#include <stdlib.h>
+
+//es werden zwei Parameter an calculatorAdd bzw Sub übergeben, und es gibt einen Ausgabewert
+
+extern void bubblesort();
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
---
+++ Semester1/Aufgaben/Rechnerarchitektur/Uebung8/bubblesort_.asm
@@ -0,0 +1,10 @@
+; Fabien Geeraert und Ailis Oßwald, Tutorium 02 bei Christoph Van Heteren-Frese
+
+section .text
+
+; veröffentlichen der operatoren, des ergebnis und funktionseinsprünge
+
+global bubblesort
+
+buublesort:
+	ret
